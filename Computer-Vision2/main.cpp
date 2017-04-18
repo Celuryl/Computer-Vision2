0a808489972ee0e380ebf7ecd3b49e02a8b551a6
Celuryl <Celuryl@gmail.com>
2017-04-18 10:36:38 +0200

    Initial Project commit + OpenCV310Extras Property sheets

---
+++ Computer-Vision2/main.cpp
@@ -0,0 +1,11 @@
+#include "opencv2/core.hpp"
+#include "opencv2/imgproc.hpp"
+#include "opencv2/highgui.hpp"
+#include <opencv2/opencv.hpp>
+#include <iostream>
+#include <fstream>
+
+int main(int argc, char* argv[])
+{
+		
+}
